2ca6f61d671bfb654d5f6d153142421546a719ff
Ryan Bartling <rbartling@axon.com>
2025-10-06 14:23:08 -0700

    ci: Update dependencies, get working on windows

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,7 +1,9 @@
 include_guard()
 
+cpmusepackagelock(package-lock.cmake)
+
 if(FEATURE_TESTS)
     if(NOT TARGET Catch)
-        cpmaddpackage("gh:catchorg/Catch2@3.7.1")
+        cpmaddpackage("gh:catchorg/Catch2@3.11.0")
     endif()
 endif()
